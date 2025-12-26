4100dd5f0d31f0ed8535923f5cb0868ed41cc881
Le Phan <lvphan@outlook.com>
2025-12-25 19:22:29 -0800

    Update D12_Number_Guessing.py
    I moved around some code because it made more sense this way than how I previously had it.
    -> Trying to edit global variables
    -> Some print statements and difficulty input outside of the guess_the_number function

--- D12_Number_Guessing.py
+++ D12_Number_Guessing.py
@@ -1,18 +1,18 @@
 import random
 import art
 
-difficulty = ""
-attempts = 0
-
 def guess_the_number():
-    global attempts
-    global difficulty
+    attempts = 0
+
+    print(art.logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    difficulty = (input("Choose a difficulty: Type 'easy' or 'hard': ")).lower()
     if difficulty == "easy":
         attempts = 10
-        print(f"You have {attempts} attempts remaining to guess the number.")
     elif difficulty == "hard":
         attempts = 5
-        print(f"You have {attempts} attempts remaining to guess the number.")
+    print(f"You have {attempts} attempts remaining to guess the number.")
     number = random.randint(1, 100)
     guess = int(input("Guess the number: "))
     while guess != number:
@@ -30,9 +30,5 @@ def guess_the_number():
             guess = int(input("Guess the number: "))
 
     print(f"You got it! The answer was {number}")
-
-print(art.logo)
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-difficulty = (input("Choose a difficulty: Type 'easy' or 'hard': ")).lower()
+    
 guess_the_number()
