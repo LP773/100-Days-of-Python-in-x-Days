b889a26ab066476b73c720caca7bcc742fa976d6
Le Phan <lvphan@outlook.com>
2025-12-25 19:40:02 -0800

    Update D12_Number_Guessing.py
    The while loop condition I had previously (while guess != number) didn't make as much sense as tracking attempts. I also realized there was no real win/lose condition.

--- D12_Number_Guessing.py
+++ D12_Number_Guessing.py
@@ -6,28 +6,34 @@ def guess_the_number():
 
     print(art.logo)
     print("Welcome to the Number Guessing Game!")
+    number = random.randint(1, 100)
     print("I'm thinking of a number between 1 and 100.")
     difficulty = (input("Choose a difficulty: Type 'easy' or 'hard': ")).lower()
+    
     if difficulty == "easy":
         attempts = 10
     elif difficulty == "hard":
         attempts = 5
+        
     print(f"You have {attempts} attempts remaining to guess the number.")
-    number = random.randint(1, 100)
+    
+    while attempts > 0:
     guess = int(input("Guess the number: "))
-    while guess != number:
         if guess < number:
             print("\nToo low")
             print("Guess again")
             attempts -= 1
             print("You have " + str(attempts) + " attempts remaining to guess the number.\n")
-            guess = int(input("Guess the number: "))
         elif guess > number:
             print("\nToo high")
             print("Guess again")
             attempts -= 1
             print("You have " + str(attempts) + " attempts remaining to guess the number.\n")
-            guess = int(input("Guess the number: "))
+        else:
+            print(f"You got it! The answer was {number}")
+            break
+    if attempts == 0:
+        print("You've run out of guesses, you lose.")
 
     print(f"You got it! The answer was {number}")
     
