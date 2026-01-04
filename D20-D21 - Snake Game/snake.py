11320bd371ca730a21d089f55995e3fb0b88c154
Le Phan <lvphan@outlook.com>
2026-01-03 17:24:02 -0800

    Update snake.py
    Refactor snake creation and growth methods

--- D20-D21 - Snake Game/snake.py
+++ D20-D21 - Snake Game/snake.py
@@ -6,6 +6,9 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
 class Snake:
     def __init__(self):
         self.segments = []
@@ -14,11 +17,18 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def grow(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -34,9 +44,11 @@ class Snake:
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
+
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
+
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
