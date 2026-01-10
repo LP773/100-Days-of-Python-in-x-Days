a72d8e53a4ffc56d260c0ee2f9ca1d6e9856ac06
Le Phan <lvphan@outlook.com>
2026-01-10 13:45:54 -0800

    Refactor code for a Paddle class

--- D22 - Pong Game/main.py
+++ D22 - Pong Game/main.py
@@ -7,27 +7,16 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-#player_paddle = Paddle()
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.penup()
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.setpos(350,0)
-
-def move_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def move_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+player1_paddle = Paddle(x_cor=-350, y_cor=0)
+player2_paddle = Paddle(x_cor=350, y_cor=0)
 
 screen.listen()
-screen.onkey(move_up, "Up")
-screen.onkey(move_down, "Down")
-game_on = True
+screen.onkey(player1_paddle.move_w, "w")
+screen.onkey(player1_paddle.move_s, "s")
+screen.onkey(player2_paddle.move_up, "Up")
+screen.onkey(player2_paddle.move_down, "Down")
 
+game_on = True
 while game_on:
     screen.update()
 
