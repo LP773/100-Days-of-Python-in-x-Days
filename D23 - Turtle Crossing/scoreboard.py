058136575e7d3506ee173de39bc5c9596c88a2c5
Le Phan <lvphan@outlook.com>
2026-01-12 15:45:54 -0800

    Update scoreboard.py

--- D23 - Turtle Crossing/scoreboard.py
+++ D23 - Turtle Crossing/scoreboard.py
@@ -10,4 +10,13 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.color("black")
         self.goto(-235,270)
+        self.score_msg = self.write(f"Level: {self.score}", align="center", font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
         self.write(f"Level: {self.score}", align="center", font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER!", align="center", font=FONT)
