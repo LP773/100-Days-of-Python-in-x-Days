3116c3aedf39a0ac73cbb9e974aae41b9e839abc
Le Phan <lvphan@outlook.com>
2026-01-12 15:45:14 -0800

    Update main.py

--- D23 - Turtle Crossing/main.py
+++ D23 - Turtle Crossing/main.py
@@ -14,9 +14,30 @@ player = Player()
 scoreboard = Scoreboard()
 car = Car()
 
+difficulty = 10
+
+screen.listen()
+screen.onkey(player.move, "Up")
+screen.onkey(player.move, "w")
+
 game_over = False
 while not game_over:
     screen.update()
     time.sleep(0.1)
+
+    car.load_car()
+    car.move_cars()
+
+    # Scoring/Finish Line Detection
+    if player.ycor() > 280:
+        player.reset_position()
+        car.increase_speed()
+        scoreboard.increase_score()
+
+    # Detecting Collision
+    for cars in car.all_cars:
+        if cars.distance(player) < 25:
+            game_over = True
+            scoreboard.game_over()
 
 screen.exitonclick()
