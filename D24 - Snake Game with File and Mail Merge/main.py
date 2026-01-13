fd6b0ea7d181295ddd29c885656d51ffe33ac14c
Le Phan <lvphan@outlook.com>
2026-01-13 12:06:05 -0800

    Create main.py

---
+++ D24 - Snake Game with File and Mail Merge/main.py
@@ -0,0 +1,48 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect Collision with Food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.grow()
+        scoreboard.increase_score()
+
+    # Detect Collision with Wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        scoreboard.reset()
+        snake.reset()
+        # scoreboard.game_over()
+
+    # Detect Collision with Self
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+            # scoreboard.game_over()
+
+screen.exitonclick()
