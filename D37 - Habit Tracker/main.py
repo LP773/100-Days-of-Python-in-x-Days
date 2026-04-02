f4f506c32e5922210a6d42305e282c038297429a
Le Phan <lphan@duck.com>
2026-04-01 17:11:55 -0700

    Originally turned all the different API calls to different methods that required inputs. To make it easier to call these methods, I added input prompts.

--- D37 - Habit Tracker/main.py
+++ D37 - Habit Tracker/main.py
@@ -25,8 +25,9 @@ headers = {
     "X-USER-TOKEN": os.getenv("TOKEN"),
 }
 
+
 def create_user():
-    response = requests.post(pixela_user_api,json=user_parameters)
+    response = requests.post(pixela_user_api, json=user_parameters)
     print(response.text)
 
 def create_graph():
@@ -35,31 +36,32 @@ def create_graph():
     response = requests.post(pixela_graph_api, json=graph_config, headers=headers)
     print(response.text)
 
-def add_pixel(quantity: str):
+def add_pixel():
+    date = input("What date are you trying to add? (Format: YYYYmmDD): ")
+    amount = input("How many times did you eat today?: ")
     pixela_add_api = f"{pixela_user_api}/{USERNAME}/graphs/{graph_config['id']}"
-    yesterday = datetime.datetime(year=2026, month=3, day=31)
     body = {
-        "date": yesterday.strftime("%Y%m%d"),
-        "quantity": quantity,
+        "date": date,
+        "quantity": amount,
     }
 
     response = requests.post(pixela_add_api, json=body, headers=headers)
     print(response.text)
 
-def update_pixel(date_input, quantity: str):
-    date = date_input
-    pixela_update_api = f"{pixela_user_api}/{USERNAME}/graphs/{graph_config['id']}/{date.strftime('%Y%m%d')}"
+def update_pixel():
+    date = input("What date are you trying to update? (Format: YYYYmmDD): ")
+    amount = input(f"How many times did you ACTUALLY eat on {date}?: ")
+    pixela_update_api = f"{pixela_user_api}/{USERNAME}/graphs/{graph_config['id']}/{date}"
     body = {
-        "quantity": quantity,
+        "quantity": amount,
     }
 
     response = requests.put(pixela_update_api, json=body, headers=headers)
     print(response.text)
 
-def delete_pixel(date_input):
-    date = date_input
-    pixela_delete_api = f"{pixela_user_api}/{USERNAME}/graphs/{graph_config['id']}/{date.strftime('%Y%m%d')}"
+def delete_pixel():
+    date = input("What date are you embarrassed about? (Format: YYYYmmDD): ")
+    pixela_delete_api = f"{pixela_user_api}/{USERNAME}/graphs/{graph_config['id']}/{date}"
 
     response = requests.delete(pixela_delete_api, headers=headers)
     print(response.text)
-    
