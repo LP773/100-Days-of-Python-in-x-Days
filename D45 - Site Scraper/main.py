71513f41e7e7bfabf40875790b542015d0c1be33
Le Phan <lphan@duck.com>
2026-04-03 16:41:18 -0700

    Scrape an web archive of an Empire article of top 100 movies for the 100 movies and write to a text file

--- D45 - Site Scraper/main.py
+++ D45 - Site Scraper/main.py
@@ -1,29 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-y_combinator_url = "https://news.ycombinator.com/news"
-response = requests.get(y_combinator_url)
-yc = response.text
+destination_url = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+response = requests.get(destination_url)
+content = response.text
 
-soup = BeautifulSoup(yc, "html.parser")
-title_lines = soup.find_all(name='span', class_='titleline')
-subline_lines = soup.find_all(name='span', class_="score")
+soup = BeautifulSoup(content, "html.parser")
 
-titles = [title.text for title in title_lines]
-links = [title.a['href'] for title in title_lines]
-scores = [int(score.text.split()[0]) for score in subline_lines]
+scrapped_titles = soup.find_all(name="h3", class_="title")
+movie_titles = [title.text for title in scrapped_titles]
+movie_titles.reverse()
 
-for item in range(len(titles) - 1):
-    print(titles[item])
-    print(links[item])
-    print(scores[item])
-    print("")
-
-highest_score = max(scores)
-index = scores.index(highest_score)
-print("====================================")
-print("THE HIGHEST SCORE IS", highest_score)
-print(titles[index])
-print(links[index])
-print(scores[index])
+with open("movies.txt", 'w') as file:
+    for item in movie_titles:
+        file.write(item + "\n")
 
