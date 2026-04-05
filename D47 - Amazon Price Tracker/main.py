ce4b4d7f0fdea527f3b7f97faad4874d31a671f5
Le Phan <lphan@duck.com>
2026-04-05 09:56:22 -0700

    - Used BeautifulSoup to scrape Amazon and instead of using smtp to send an email alert per the course. I used Telegram's API to send messages to myself from a bot instead! - Removed an extra space in folder title for D35

---
+++ D47 - Amazon Price Tracker/main.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+# TELEGRAM
+# https://api.telegram.org/bot<token>/METHOD_NAME
+API_ENDPOINT = "https://api.telegram.org/bot"
+TOKEN = os.environ['TELEGRAM_TOKEN']
+
+# AMAZON
+item_url = "https://www.amazon.com/dp/B0D8694QGZ?ie=UTF8&th=1"
+headers = {
+    "Accept-Language": "en-US",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:149.0) Gecko/20100101 Firefox/149.0"
+}
+
+# Scrape
+threshold = 30
+request = requests.get(item_url, headers=headers)
+soup = BeautifulSoup(request.content, 'html.parser')
+product = soup.find(name="span", id="productTitle").get_text().strip()
+whole = soup.find(name="span", class_="a-price-whole")
+fraction = soup.find(name="span", class_="a-price-fraction")
+price = float(whole.text + fraction.text)
+
+# Send Telegram message
+if price < threshold:
+    message = (f"""
+The item you're watching 
+<a href="{item_url}">{product}</a> 
+has fallen <b><i>BELOW</i></b> the price threshold of <i>{threshold}</i>.
+
+It is currently <b>{price}</b>.
+""")
+    parameters = {
+        "chat_id": os.environ["TELEGRAM_CHAT_ID"],
+        "text": message,
+        "parse_mode": "HTML"
+    }
+    yaab = requests.post(f"{API_ENDPOINT}{TOKEN}/sendMessage", params=parameters)
+    print(yaab.status_code, yaab.text)
+
