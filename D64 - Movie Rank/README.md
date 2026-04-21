614ee0816bc76da733f994b41a9818843103eedf
Le Phan <lphan@duck.com>
2026-04-20 22:13:05 -0700

    Complete requirement 1 and update README with a list of requirements

--- D64 - Movie Rank/README.md
+++ D64 - Movie Rank/README.md
@@ -1,6 +1,14 @@
+# Movie Rank
 In this project, I will be creating a Movie Ranking website utilizing
 - Flask
 - WTForms
 - SQLite
 - SQLAlchemy
 and more
+
+## Requirements
+- [X] Be able to view movie list items 
+- [ ] Be able to edit a movie's rating
+- [ ] Be able to delete movies from the database
+- [ ] Be able to add new movies via the Add page
+- [ ] Be able to sort and rank movies by rating
--- D64 - Movie Rank/main.py
+++ D64 - Movie Rank/main.py
@@ -23,17 +23,35 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 Bootstrap5(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+  pass
 
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    year = db.Column(db.Integer)
+    description = db.Column(db.String(500))
+    rating = db.Column(db.Float)
+    ranking = db.Column(db.Integer)
+    review = db.Column(db.String(500))
+    img_url = db.Column(db.String(500))
 
 # CREATE TABLE
-
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = Movie.query.all()
+    return render_template("index.html", movie_list=movies)
 
 
 if __name__ == '__main__':
