4f162caf98a413725ee2f267dc2d698ede0d3672
Le Phan <lphan@duck.com>
2026-04-29 22:06:48 -0700

    Complete requirement 1 and add README for day 67

---
+++ D67 - Blog Capstone - RESTful Routing/README.md
@@ -0,0 +1,8 @@
+# RESTful Routing - Blog Capstone Part 3
+A blog to HTTP routes to create, edit, and delete blog posts.
+## Requirements
+- [X] Be able to GET blog post items
+- [ ] Be able to POST a new blog post
+- [ ] Be able to edit existing blog posts
+- [ ] Be able to DELETE blog posts
+
--- D67 - Blog Capstone - RESTful Routing/main.py
+++ D67 - Blog Capstone - RESTful Routing/main.py
@@ -9,19 +9,6 @@ from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
@@ -33,7 +20,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-
 # CONFIGURE TABLE
 class BlogPost(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -44,15 +30,23 @@ class BlogPost(db.Model):
     author: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
-
 with app.app_context():
     db.create_all()
 
-
 @app.route('/')
 def get_all_posts():
-    # TODO: Query the database for all the posts. Convert the data to a python list.
+    all_posts = db.session.execute(db.select(BlogPost)).scalars().all()
     posts = []
+    for post in all_posts:
+        posts.append({
+            'id': post.id,
+            'title': post.title,
+            'subtitle': post.subtitle,
+            'date': post.date,
+            'body': post.body,
+            'author': post.author,
+            'img_url': post.img_url
+        })
     return render_template("index.html", all_posts=posts)
 
 # TODO: Add a route so that you can click on individual posts.
@@ -61,7 +55,6 @@ def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
     requested_post = "Grab the post from your database"
     return render_template("post.html", post=requested_post)
-
 
 # TODO: add_new_post() to create a new blog post
 
@@ -74,11 +67,9 @@ def show_post(post_id):
 def about():
     return render_template("about.html")
 
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
