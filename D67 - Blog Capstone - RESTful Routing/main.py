e11ded5ce90dfb916253245faff90dedd90b1948
Le Phan <lphan@duck.com>
2026-05-01 14:03:15 -0700

    Complete a part of requirement 1 that was missed. I forgot to do the show_post method that gives the ability to click on an individual post.

--- D67 - Blog Capstone - RESTful Routing/main.py
+++ D67 - Blog Capstone - RESTful Routing/main.py
@@ -59,11 +59,9 @@ def get_all_posts():
         })
     return render_template("index.html", all_posts=posts)
 
-# TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route('/post/<int:post_id>', methods=['GET'])
 def show_post(post_id):
-    # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    requested_post = db.session.get(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
 
 # TODO: add_new_post() to create a new blog post
