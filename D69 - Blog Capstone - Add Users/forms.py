db743758a21d1472c13d3f1bd44f2c66320af9f6
Le Phan <lphan@duck.com>
2026-05-09 15:10:52 -0700

    update: Add RegisterForm and LoginForm

--- D69 - Blog Capstone - Add Users/forms.py
+++ D69 - Blog Capstone - Add Users/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
-
 
 # WTForm for creating a blog post
 class CreatePostForm(FlaskForm):
@@ -12,11 +11,17 @@ class CreatePostForm(FlaskForm):
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
-
 # TODO: Create a RegisterForm to register new users
-
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # TODO: Create a LoginForm to login existing users
-
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # TODO: Create a CommentForm so users can leave comments below posts
